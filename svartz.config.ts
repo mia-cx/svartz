60cf8d314d85a9d29ec478781a957a7d024b3fba
mia-riezebos <mia@mia.cx>
2026-03-09 15:06:02 +0100

    chore(svartz): static target and docs/obsidian-journal/vault entries

--- svartz.config.ts
+++ svartz.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from "@svartz/config";
 
 export default defineConfig({
-  version: "0.0.1",
-  defaults: {},
+  version: "1.0.0",
+  defaults: {
+    theme: "@svartz/theme-minimal",
+  },
   vaults: [
     {
       id: "docs",
       path: "vaults/docs",
       target: {
-        type: "cloudflare-workers",
-        name: "docs",
+        type: "static",
+      },
+    },
+    {
+      id: "obsidian-journal",
+      path: "vaults/obsidian-journal",
+      target: {
+        type: "static",
+      },
+    },
+    {
+      id: "vault",
+      path: "vaults/vault",
+      target: {
+        type: "static",
       },
     },
   ],
